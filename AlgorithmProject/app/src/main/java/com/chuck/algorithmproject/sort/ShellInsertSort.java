6e279f9b87eb753dd85c7479b0bdcc27f957d103
陈刚 <chuck@192.168.0.107>
2020-07-30 23:03:53 +0800

    优化希尔排序算法

--- AlgorithmProject/app/src/main/java/com/chuck/algorithmproject/sort/ShellInsertSort.java
+++ AlgorithmProject/app/src/main/java/com/chuck/algorithmproject/sort/ShellInsertSort.java
@@ -6,28 +6,32 @@ import java.util.Arrays;
 
 public class ShellInsertSort {
     private static String TAG = "ShellInsertSort";
-    //        private static int[] number = {5, 8, 3, 6, 9, 10, 34, 1, 7, 6};
+    private static int[] number = {5, 8, 3, 6, 9, 10, 34, 1, 7, 6};
 //    private static int[] number = {5, 8, 3, 6, 9, 10, 34, 1, 7, 12, 65, 4, 23, 2, 16, 15, 76, 56, 8};
-    private static int[] number = {5, 8, 3, 6, 9, 10, 34, 1, 7, 12, 65, 4, 23, 2, 16, 15, 76, 56, 8, 5, 8, 3, 6, 9, 10, 34, 1, 7, 12, 65, 4, 23, 2, 16, 15, 76, 56, 8, 6, 6, 6, 6, 6, 6, 6, 6};
+//    private static int[] number = {5, 8, 3, 6, 9, 10, 34, 1, 7, 12, 65, 4, 23, 2, 16, 15, 76, 56, 8, 5, 8, 3, 6, 9, 10, 34, 1, 7, 12, 65, 4, 23, 2, 16, 15, 76, 56, 8, 6, 6, 6, 6, 6, 6, 6, 6};
 
     public static void shellInsertSort() {
         int size = number.length;
-        int initDelta = size / 2 + 1;
-        for (int i = 0; i < initDelta; i++) {
-            int delta = size / ((i + 1) * 2);
-            delta = delta == 0 ? 1 : delta;
-            Log.d(TAG, "delta is " + delta + " , i = " + i);
-            for (int j = 0; j < size; j++) {
-                int compareIndex = j + delta;
-                if (compareIndex < size && number[compareIndex] < number[j]) {
-                    Log.d(TAG, "hold number is " + number[j] + ",compare number is " + number[compareIndex]);
-                    int temp = number[j];
-                    number[j] = number[compareIndex];
-                    number[compareIndex] = temp;
+        int delta = size;
+//        int initDelta = size / 2 + 1;
+        while (true) {
+            delta = delta / 2;
+            for (int i = 0; i < delta; i++) {
+                Log.d(TAG, "delta is " + delta + " , i = " + i);
+                for (int j = i + delta; j < size; j += delta) {
+                    int tempData = number[j];
+                    int k = j - delta;
+                    while (k >= 0 && number[k] > tempData) {
+                        number[k + delta] = number[k];
+                        k -= delta;
+                    }
+                    number[k + delta] = tempData;
                 }
             }
-
-            Log.d(TAG, "sorted number is " + Arrays.toString(number));
+            if (delta == 1) {
+                break;
+            }
         }
+        Log.d(TAG, "sorted number is " + Arrays.toString(number));
     }
 }
